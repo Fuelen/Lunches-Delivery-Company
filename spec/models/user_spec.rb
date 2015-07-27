22fdefb4c19e0c6cdcd3bd5e7b6b78cd86db615a
Fuelen <ifuelen@gmail.com>
2015-07-27 21:53:11 +0300

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:first_user) { FactoryGirl.create(:user) }
+  context "when first creating" do
+    it "is admin" do
+      expect(first_user.admin?).to be true
+    end
+
+    it "has name 'Launches Admin'" do
+      expect(first_user.name).to eq "Launches Admin"
+    end
+  end
+
+  context "when second creating" do
+    let(:second_user) { FactoryGirl.create(:other_user) }
+
+    it "is not admin" do
+      expect(second_user.admin?).to be false
+    end
+
+    it "has its name" do
+      expect(second_user.name).to eq second_user.name
+    end
+  end
 end
