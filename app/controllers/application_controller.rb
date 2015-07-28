f64b8e94a4309b0675028ac990acddaba778b49c
Fuelen <ifuelen@gmail.com>
2015-07-28 12:19:39 +0300

    Add for_admin filter
    It raises an exception when user is not signed in or
    is not admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def for_admin!
+    if !user_signed_in? or !current_user.admin?
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
 end
