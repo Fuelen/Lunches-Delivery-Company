782b7fd62fe8fc888dcee5a1c3a7559146e3d757
Fuelen <ifuelen@gmail.com>
2015-07-28 10:56:05 +0300

    Set Dish available on today before creating

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,12 @@
 class Dish < ActiveRecord::Base
   enum kind: [ :first_course, :main_course, :drink ]
+
+  before_create :set_available_on_today
+
+  private
+
+  def set_available_on_today
+    self.available_on = Date.today
+  end
+
 end
