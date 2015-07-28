a8f0c963451359fe5b79df14bf7de2635699b277
Fuelen <ifuelen@gmail.com>
2015-07-28 12:35:34 +0300

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+User.create name: "Launches Admin", email: "email@example.com",
+  password: 12345678, password_confirmation: 12345678
+p "Admin email:    email@example.com"
+p "Admin password: 12345678"
+30.times do
+  password = Faker::Internet.password
+  User.create name: Faker::Name.name, email: Faker::Internet.email,
+    password: password, password_confirmation: password
+end
