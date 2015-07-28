77aadd245cf5310a399d26a53d3f332ba108073d
Fuelen <ifuelen@gmail.com>
2015-07-28 17:36:16 +0300

    Add navigation menu for admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def show_admin_navbar(current_page = :dashboard)
+    return unless current_user.admin?
+    nav_items = { 
+      dashboard:{
+        url:  root_path,
+        name: "Dashboard",
+        icon: "dashboard"
+      },
+      users: {
+        url:  users_path,
+        name: "Users",
+        icon: "user"
+      },
+      new_dish: {
+        url:  new_dish_path,
+        name: "Make menu for today",
+        icon: "menu-hamburger"
+      },
+    }
+    content_tag :div, class: "nav nav-tabs" do
+      nav_items.map do |page, attr|
+        content_tag :li, class: ("active" if current_page == page) do
+          link_to attr[:url] do
+            "<span class='glyphicon glyphicon-#{attr[:icon]}'></span> ".html_safe + attr[:name]
+          end
+        end
+      end.reduce(&:+)
+    end
+  end
 end
