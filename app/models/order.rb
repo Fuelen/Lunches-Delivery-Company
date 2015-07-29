82f8a7877849fd0cacd763e174d51d2657e0d881
Fuelen <ifuelen@gmail.com>
2015-07-29 07:39:46 +0300

    Add relationships for Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  belongs_to :first_course, class_name: "Dish"
+  belongs_to :main_course, class_name: "Dish"
+  belongs_to :drink, class_name: "Dish"
 end
