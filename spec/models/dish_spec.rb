e66f3f6293a1d9e9bedc5ab8eb9df3d92489feff
Fuelen <ifuelen@gmail.com>
2015-07-30 19:40:16 +0300

    Add unit tests for Dish and Order

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "before create" do
+    let(:dish) { FactoryGirl.build(:dish) }
+
+    it "has price lower than 0.01" do
+      dish.price = -9.24
+      expect(dish).to be_invalid
+    end
+
+    it "has price greater than or equal to 0.01" do
+      dish.price = 2
+      expect(dish).to be_valid
+    end
+  end
+
+  context "when created" do
+    let(:dish) { FactoryGirl.create(:dish) }
+
+    it "is available on today" do
+      expect(dish.available_on).to eq Date.today
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "before creation" do
+    let(:first_course) { FactoryGirl.create :first_course }
+    let(:main_course)  { FactoryGirl.create :main_course }
+    let(:drink)        { FactoryGirl.create :drink }
+    let(:order)        { FactoryGirl.build  :order }
+
+    it "has invalid first course" do
+      order.first_course_id = main_course.id
+      expect(order).to be_invalid
+    end
+
+    it "has valid first course" do
+      order.first_course_id = first_course.id
+      expect(order).to be_valid
+    end
+
+    it "has invalid main course" do
+      order.main_course_id = first_course.id
+      expect(order).to be_invalid
+    end
+
+    it "has valid main course" do
+      order.main_course_id = main_course.id
+      expect(order).to be_valid
+    end
+
+    it "has invalid drink" do
+      order.drink_id = main_course.id
+      expect(order).to be_invalid
+    end
+
+    it "has valid drink" do
+      order.drink_id = drink.id
+      expect(order).to be_valid
+    end
+  end
+
+  describe "#created_on" do
+    let(:new_order)       { FactoryGirl.build  :order }
+    let(:persisted_order) { FactoryGirl.create :order }
+
+    it { expect(new_order.created_on).to be_nil }
+    it { expect(persisted_order.created_on).to eq Date.today }
+  end
+
+  describe "#total_price" do
+    let(:first_course)    { FactoryGirl.create :first_course, price: 10 }
+    let(:main_course)     { FactoryGirl.create :main_course,  price: 15 }
+    let(:drink)           { FactoryGirl.create :drink,        price: 5  }
+    let(:new_order)       { FactoryGirl.build(:order,
+                                              first_course: first_course,
+                                              main_course: main_course,
+                                              drink: drink) }
+    let(:persisted_order) { FactoryGirl.create(:order,
+                                               first_course: first_course,
+                                               main_course: main_course,
+                                               drink: drink) }
+
+    it { expect(new_order.total_price).to be_nil }
+    it { expect(persisted_order.total_price).to eq 30 }
+  end
+
 end
