f24ad553c1804ca610ccdf3f1fea6cedac83db04
Fuelen <ifuelen@gmail.com>
2015-07-31 20:09:10 +0300

    Add tests for API

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OrdersController, type: :controller do
+  before {  create(:order) }
 
-  describe "GET #today" do
-    it "returns http success" do
-      get :today
-      expect(response).to have_http_status(:success)
+  context "when valid token" do
+    render_views
+    describe "GET #today" do
+      before do
+        API_TOKENS << "valid token"
+        get :today, api_token: "valid token", format: :json
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns json that corresponds with schema" do
+        expect(response).to match_response_schema("orders_today")
+      end
     end
   end
 
+  context "when invalid token" do
+    describe "GET #today" do
+      before do
+        get :today, api_token: "invalid token", format: :json
+      end
+
+      it "returns http unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it "returns json that corresponds with schema" do
+        expect(response).to match_response_schema("unauthorized")
+      end
+    end
+  end
 end
